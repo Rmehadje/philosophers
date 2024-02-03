7c83a6372b03c42e49e22a2dbc48132be6c836b2
Rmehadje <106964522+Rmehadje@users.noreply.github.com>
2024-02-03 19:27:29 +0100

    Update philosophers.h 03/02

--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: rmehadje <rmehadje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 14:14:37 by rmehadje          #+#    #+#             */
-/*   Updated: 2024/01/29 17:40:36 by rmehadje         ###   ########.fr       */
+/*   Updated: 2024/02/03 19:12:56 by rmehadje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,18 +30,23 @@ typedef struct			s_phil
 	pthread_t				thread_id;
 	int						eating;
 	int						p_ate;
-	int						last_meal;
+	long long				ttd;
+	long long						last_meal;
 	int						times_to_eat;
 	int						meals_eaten;
 	int						num_of_phils;
 	int						isdead;
-	size_t					start;
-	size_t					time_to_eat;
-	size_t					time_before_death;
-	size_t					time_to_sleep;
+	long long					start;
+	long long					time_to_eat;
+	long long					time_before_death;
+	long long					time_to_sleep;
 	pthread_mutex_t		*left_f;
 	pthread_mutex_t		*locked;
 	pthread_mutex_t		*right_f;
+	pthread_mutex_t		tmp;
+	pthread_mutex_t		tmp2;
+	pthread_mutex_t		tmp3;
+	pthread_mutex_t		tmp4;
 	pthread_mutex_t		*dead_lock;
 	pthread_mutex_t		*meal_lock;
 	pthread_mutex_t		*write_lock;
@@ -55,10 +60,12 @@ typedef struct			s_data
 	int						start_time;
 	int						dead;
 	int						isdead;
+	int						ate;
 	pthread_mutex_t		write_lock;
 	pthread_mutex_t		meal_lock;
 	pthread_mutex_t		dead_lock;
 	pthread_mutex_t		forks[200];
+	pthread_t				thread[200];
 	t_phil		phils[200];
 }							t_data;
 
@@ -76,14 +83,17 @@ int		sleep_mode(size_t		MS);
 
 void		philosophing(t_phil	*phil);
 void		asleep(t_phil	*phil);
-void		munching(t_phil	*phil);
+void		*munching(void	*param);
 
 void		ft_putstr(char *str);
 int		error_messanger(int nbr);
 
-
-
-
-
+void	destroy_all(t_data	*data);
+int	death_checker(t_data		*data);
+void		death_print(t_data	*data, int philosopher_id);
+int		init_threads(t_data	*data);
+void		eating(t_phil *phil);
+int	eat_checker(t_data *data);
+void	death(t_data *data);
 
 #endif
