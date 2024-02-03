210e5cfb4b618d0ec749106d7c8cc4967f9d3004
Rmehadje <106964522+Rmehadje@users.noreply.github.com>
2024-02-03 19:26:51 +0100

    Update initializing.c 03/02

--- initializing.c
+++ initializing.c
@@ -6,9 +6,11 @@
 /*   By: rmehadje <rmehadje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 15:48:13 by rmehadje          #+#    #+#             */
-/*   Updated: 2024/01/30 18:30:48 by rmehadje         ###   ########.fr       */
+/*   Updated: 2024/02/03 19:17:58 by rmehadje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "philosophers.h"
 
 int		init_philos(t_data *data)
 {
@@ -16,8 +18,8 @@ int		init_philos(t_data *data)
 
 	final_init(data);
 	init_forks(data);
-	i = -1;
-	while (++i < data->phils[0].num_of_phils)
+	i = 0;
+	while (i < data->phils[0].num_of_phils)
 	{
 		data->phils[i].id = i + 1;
 		data->phils[i].meals_eaten = 0;
@@ -28,8 +30,16 @@ int		init_philos(t_data *data)
 		data->phils[i].write_lock = &data->write_lock;
 		data->phils[i].dead_lock = &data->dead_lock;
 		data->phils[i].dead = &data->dead;
+		i++;
+	}
+	data->phils[0].left_f = &data->forks[0];
+	data->phils[0].right_f = &data->forks[data->phils[0].num_of_phils - 1];
+	i = 1;
+	while (i < data->phils[0].num_of_phils)
+	{
 		data->phils[i].left_f = &data->forks[i];
 		data->phils[i].right_f = &data->forks[i - 1];
+		i++;
 	}
 	return (0);
 }
@@ -62,6 +72,7 @@ int		init_args(char **argv)
 		data->phils[i].time_before_death = ft_atoi(argv[2]);
 		data->phils[i].time_to_eat = ft_atoi(argv[3]);
 		data->phils[i].time_to_sleep = ft_atoi(argv[4]);
+		data->phils[i].data = data;
 		if (argv[5])
 			data->phils[i].times_to_eat = ft_atoi(argv[5]);
 		else
@@ -69,27 +80,59 @@ int		init_args(char **argv)
 		i++;
 	}
 	init_philos(data);
+	init_threads(data);
 	return (0);
 }
 
 int		init_threads(t_data	*data)
 {
 	int i;
+	int	f;
+
+	f = 0;
+	i = 0;
+	while (i < data->phils[i].num_of_phils)
+	{
+		data->phils[i].start = ft_real_time();
+		data->phils[i].last_meal = ft_real_time();
+		data->phils[i].ttd = data->phils[i].time_before_death;
+		i++;
+	}
+	i = 0;
+	while (i < data->phils[i].num_of_phils)
+	{
+		if (pthread_create(&(data->thread[i]), NULL, &munching, &data->phils[i]) == 1)
+			return (0);
+		i++;
+	}
+	if (death_checker(data) == 1)
+		pthread_mutex_unlock(&data->write_lock);
+	i = 0;
+	while (i <data->phils[i].num_of_phils)
+	{
+		pthread_join(data->thread[i], NULL);
+		i++;
+	}
+	destroy_all(data);
+	return (0);
+}
+
+void	destroy_all(t_data	*data)
+{
+	int i;
 
 	i = 0;
-	while (i <data->phils[i].num_of_phils)
+	while (i < data->phils[i].num_of_phils)
 	{
-		pthread_create(&data->phils[i].id, NULL, &munching, &data->phils[i]);
+		pthread_mutex_destroy(&data->forks[i]);
+		pthread_mutex_destroy(&data->phils[i].tmp);
+		pthread_mutex_destroy(&data->phils[i].tmp2);
+		pthread_mutex_destroy(&data->phils[i].tmp3);
 		i++;
 	}
-	death_checker(data);
-	i = 0;
-	while (i <data->phils[i].num_of_phils)
-	{
-		pthread_join(&data->phils[i], NULL);
-		i++;
-	}
-	return (0);
+	pthread_mutex_destroy(&data->write_lock);
+	pthread_mutex_destroy(&data->meal_lock);
+	pthread_mutex_destroy(&data->dead_lock);
 }
 
 int	final_init(t_data	*data)
